8ec586adb432764542a4154acdb4a60423f3e837
SandipChakraborty <catch2sandy007@gmail.com>
2024-12-14 18:54:42 +0530

    Added cron job for trades

--- app.py
+++ app.py
@@ -78,12 +78,11 @@ def get_history_nifty_atm_ce():
 
 
 def my_cron_job():
-    # Code to be executed by the cron job
-    print('Hello from my cron job!')
+    send_message('Test schedulers!')
 
 scheduler.add_job(
     func=my_cron_job,
-    trigger=CronTrigger(hour=18, minute=52, timezone='Asia/Kolkata'),
+    trigger=CronTrigger(hour=19, minute=1, timezone='Asia/Kolkata'),
 )
 
 # Start the scheduler
@@ -91,5 +90,5 @@ scheduler.start()
 
 
 # Befor pushing to git please comment the below 2 lines
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
--- telegram_notification.py
+++ telegram_notification.py
@@ -3,12 +3,7 @@ import os
 
 def callBot(msg):
     token = os.environ.get('TOKEN')
-    print("Chat ID: {}".format(token))
     chatId = os.environ.get('CHAT_ID')
-
-    
-
     # url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
     url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chatId}&text={msg}"
-    
     return requests.get(url).json()
